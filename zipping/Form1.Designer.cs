196d500828bbd0040489d262ef235a3eaaedc3d2
1Adept1 <viktorfedorov068@gmail.com>
2020-08-21 04:50:59 +0300

    first commit

---
+++ zipping/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace zipping
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+         protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(286, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сохранить архив";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(158, 75);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(140, 23);
+            this.Delete.TabIndex = 0;
+            this.Delete.Text = "Удалить файл";
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            ".txt",
+            ".doc"});
+            this.comboBox1.Location = new System.Drawing.Point(158, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 115);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "vscode.ru";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ zipping/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace zipping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+   
+        private const string FILE = "selectedDirectory.txt";
+
+        FolderBrowserDialog bd = new FolderBrowserDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            if (bd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = bd.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Zip files (*.zip)|*.zip";
+            if (textBox1.Text!="" && sfd.ShowDialog() == DialogResult.OK)
+            {
+                ZipFile zf = new ZipFile(sfd.FileName);
+                zf.AddDirectory(bd.SelectedPath);
+                zf.Save();
+                MessageBox.Show("Архивация прошла успешно.", "Выполнено");
+            }           
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            string directoryPath = textBox1.Text; // заносим в переменную выбранный каталог
+            if (directoryPath != string.Empty) //проверяем что путь до каталога не пустой
+            {
+                if (DialogResult.Yes == MessageBox.Show(string.Format("Подтвердите очистку каталога {0}", directoryPath),
+                    "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question)) //подтверждение об очистке.
+                {
+                    DeleteAllFilesOnDirectory(directoryPath); // выполняем очистку
+                    MessageBox.Show("Операция завершена", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information); // сообщаем о завершении очистки
+                }
+            }
+        }
+        private void ErrorBox(Exception e)
+        {
+            string exception = string.Format("{0}\n{1}", e.Message, e.StackTrace);
+            MessageBox.Show(exception, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void Main(string[] args)
+        {
+            string directoryPath = textBox1.Text;
+            string _del = comboBox1.Text;
+            string[] _files = Directory.GetFiles(directoryPath, _del);
+            foreach (string fl in _files)
+            {
+                File.Delete(fl);         
+            }
+        }
+
+        private void DeleteAllFilesOnDirectory(string directoryPath)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath); //загружаем в экземпляр класса DirectroyInfo информацию о всех файлах
+            foreach (DirectoryInfo directory in directoryInfo.GetDirectories()) //получаем список всех подкаталогов в выбранном каталоге
+            {
+                try
+                {
+                    directory.Delete(true); // пытаемся удалить каталог
+                }
+                catch (Exception e)
+                {
+                    ErrorBox(e); // если не удалось, выводим сообщение об ошибке
+                }
+            }
+            foreach (FileInfo file in directoryInfo.GetFiles()) //получаем список всех файлов в выбранном каталоге
+            {
+                try
+                {
+                    file.Delete(); //пытаемся удалить файл
+                }
+                catch (Exception e)
+                {
+                    ErrorBox(e); // если не удалось, выводим сообщение об ошибке
+                }
+            }
+        }
+
+        
+    }
+}
